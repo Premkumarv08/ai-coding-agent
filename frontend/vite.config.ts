f93240f15f2fb669292749ffb3060bb5a1bbac36
Prem Kumar <premkumarv08@gmail.com>
2025-08-12 13:16:25 +0530

    Fetching API target from ENV

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target:  process.env.VITE_API_TARGET || 'http://localhost:8000',
-        changeOrigin: true,
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    server: {
+      port: 5173,
+      proxy: {
+        "/api": {
+          target: env.VITE_API_TARGET || "http://localhost:8000",
+          changeOrigin: true,
+        },
       },
     },
-  },
-})
+  };
+});
